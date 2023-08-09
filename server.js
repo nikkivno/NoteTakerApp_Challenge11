aea960361577e40e9d3bd6c551e72fd712799321
nikkivno <nikkivigneault@gmail.com>
2023-08-09 19:06:40 -0300

    changed path back to index in notes.html

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path')
 
 const PORT = 3001;
-
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+res.sendFile(path.join('/public/notes.html'))
+);
 
 // GET Request for notes
 
