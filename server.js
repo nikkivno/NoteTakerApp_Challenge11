4f61415b462813d0345e51a6e16d52fd406ab5bf
nikkivno <nikkivigneault@gmail.com>
2023-08-21 18:11:32 -0300

    updated port for heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const path = require('path')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
